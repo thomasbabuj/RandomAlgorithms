1f0b60d07647e1ff54c71a7fe1e08b23b20a5532
Thomas Babu <joseph.thomas.babu@gmail.com>
2016-02-17 11:55:09 +0800

    included a readme file

--- angry-children/angry_children.go
+++ angry-children/angry_children.go
@@ -1,18 +1,5 @@
 /**
-Bill Gates is on one of his philanthropic journeys to a village in Utopia.
-He has N packets of candies and would like to distribute one packet to each of
-the K children in the village (each packet may contain different number of
-candies). To avoid any fighting among the children, he would like to pick
-K out of N packets, such that unfairness is minimized.
-
-Suppose the K packets have (x1, x2, x3,....xk) candies in them, where xi
-denotes the number of candies in the ith packet, then we define unfairness as
-
-max(x1,x2,...xk) - min(x1,x2,...xk)
-
-where max denotes the highest value amongst the elements, and min denotes
-the least value amongst the elements. Can you figure out the minimum
-unfairness and print it?
+Minimum Possible Unfairness Value
 */
 
 package main
@@ -67,7 +54,6 @@ func main() {
 		newUnfairness = max - min
 		if newUnfairness < unfairness {
 			fmt.Println("Minimum Possible Unfairness Value ", unfairness)
-			return
 		}
 	}
 
